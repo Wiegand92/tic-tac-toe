2be435431da6444d1d9132f7a751608b4f93df38
wiegand92 <wiegand.alex@gmail.com>
2021-05-13 13:16:40 -0400

    fix line endings, update package.json

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 // 3rd Party //
 import React from 'react';
 import reactDOM from 'react-dom';
-
 // Styles //
-  // Import tailwind first to give our styles preference //
+// Import tailwind first to give our styles preference //
 import 'tailwindcss/tailwind.css';
 import './styles/style.scss';
-
 // Components //
 import App from './components/App';
-
 // Render //
 reactDOM.render(<App />, document.getElementById('app'));
